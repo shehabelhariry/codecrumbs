09cb2847bc923fe19db2f9d76aace231af99877c
Shehab ElHariry <cheha6@gmail.com>
2019-08-27 00:47:18 +0200

    added our work page

---
+++ src/pages/our-work.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const OurWork = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Our Work</h1>
+        <p>This is Our Work</p>
+      </div>
+    </Layout>
+  )
+}
+
+export default OurWork
