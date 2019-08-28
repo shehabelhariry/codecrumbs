05ab7f62ee71e7f12bf4463125e13e8b2bc21bb2
Shehab Elhariry <shehab_elhariry@Shehabs-MacBook-Pro.local>
2019-08-28 18:37:03 +0200

    styled the blog post page

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,17 +1,18 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
-// import blogTempateStyles from "./blog-template.module.scss"
+import styles from "./blog-template.module.scss"
 
 export const query = graphql`
   query($slug: String!) {
     contentfulBlogPost(slug: { eq: $slug }) {
       title
-      date
+      date(formatString: "DD MMMM YYYY")
       slug
       body {
-        body
+        json
       }
     }
   }
@@ -19,17 +20,32 @@ export const query = graphql`
 
 const Blog = props => {
   const { data } = props
-  console.log(props)
+  console.log(
+    data,
+    documentToReactComponents(data.contentfulBlogPost.body.json)
+  )
   return (
     <Layout>
-      <div>
-        <h1>{data.contentfulBlogPost.title}</h1>
-        <p>{data.contentfulBlogPost.date}</p>
-        {/* <div
-          dangerouslySetInnerHTML={{
-            __html: data.contentfulBlogPost.body.content.content.value,
-          }}
-        ></div> */}
+      <div className={styles.container}>
+        <div className={styles.blogTemplate}>
+          <h1>{data.contentfulBlogPost.title}</h1>
+          <p>{data.contentfulBlogPost.date}</p>
+          <div className={styles.blog}>
+            {documentToReactComponents(data.contentfulBlogPost.body.json, {
+              renderNode: {
+                "embedded-asset-block": node => {
+                  return (
+                    <img
+                      class="img-fluid"
+                      src={`${node.data.target.fields.file["en-US"].url}`}
+                    />
+                  )
+                },
+              },
+            })}
+          </div>
+        </div>
+        <div></div>
       </div>
     </Layout>
   )
