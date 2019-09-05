529af00f9d3e8e909ce55e07dd6f4ace53a8c126
Shehab Elhariry <shehab_elhariry@Shehabs-MacBook-Pro.local>
2019-09-05 19:29:43 +0200

    added about page

---
+++ src/components/TeamMember/TeamMember.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react"
+import Avatar from "avataaars"
+import "./team-member.scss"
+
+import LinkedInImage from "../../images/social-linkedin-circular-button.svg"
+import GithubImage from "../../images/github.svg"
+import twitterImg from "../../images/twitter.svg"
+
+const TeamMember = ({
+  name,
+  avatarProps,
+  about,
+  twitter,
+  linkedIn,
+  github,
+}) => {
+  const [eyeBrows, setEyeBrows] = useState("Default")
+  const [mouth, setMouth] = useState("Default")
+  const changeExpression = () => {
+    if (eyeBrows === "Default") {
+      setEyeBrows("RaisedExcited")
+      setMouth("Smile")
+    } else {
+      setEyeBrows("Default")
+      setMouth("Default")
+    }
+  }
+  return (
+    <div className="team-member">
+      <Avatar {...avatarProps} eyebrowType={eyeBrows} mouthType={mouth} />
+      <h3>{name}</h3>
+      <p>{about}</p>
+      <div className="team-member__icons">
+        <a
+          onMouseEnter={changeExpression}
+          onMouseLeave={changeExpression}
+          href={linkedIn}
+        >
+          <img src={LinkedInImage} />
+        </a>
+        <a
+          href={github}
+          onMouseEnter={changeExpression}
+          onMouseLeave={changeExpression}
+        >
+          <img src={GithubImage} />
+        </a>
+        <a
+          href={twitter}
+          onMouseEnter={changeExpression}
+          onMouseLeave={changeExpression}
+        >
+          <img src={twitterImg} />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default TeamMember
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,32 @@
 import React from "react"
-import Footer from "../components/footer"
-import Header from "../components/Header"
 import Layout from "../components/Layout"
+import TeamMember from "../components/TeamMember/TeamMember"
 
 const About = () => {
   return (
     <Layout>
       <div className="width-container">
-        <h1 className="page-title">About</h1>
-        <p>Bio</p>
+        <h1 className="page-title">Team/contributors</h1>
+        <TeamMember
+          avatarProps={{
+            avatarStyle: "Circle",
+            topType: "ShortHairShortFlat",
+            accessoriesType: "Prescription02",
+            hairColor: "BrownDark",
+            facialHairType: "Blank",
+            clotheType: "CollarSweater",
+            clotheColor: "Blue03",
+            eyeType: "Default",
+            eyebrowType: "Default",
+            mouthType: "Default",
+            skinColor: "Light",
+          }}
+          name="Shehab Elhariry"
+          about="Front-end Developer - (1/8) Piano Player - Comet"
+          linkedIn="https://www.linkedin.com/in/shehabelhariry/"
+          twitter="https://twitter.com/cheha6"
+          github="https://github.com/shehabelhariry"
+        />
       </div>
     </Layout>
   )
